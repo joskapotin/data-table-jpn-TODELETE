1ef483fd2fcb297885832a6cc3dd747beeadfba0
joska <jpotin@gmail.com>
2022-06-26 11:32:26 +0200

    ts again

--- src/lib/contexts/DataTableContext.tsx
+++ src/lib/contexts/DataTableContext.tsx
@@ -1,22 +1,24 @@
 import { createContext, useContext, useReducer, useMemo } from "react"
 import reducer, { initialState } from "../reducer/reducer"
-import type { DataTableState, Config } from "../types"
+import type { DataTableState, Config, DataTableAction } from "../types"
+
+type DataTableContextType = [state: DataTableState, dispatch: React.Dispatch<DataTableAction>]
 
 type DataTableContextProviderProps = {
   config: Config
   children: React.ReactNode
 }
 
-const DataTableContext = createContext<DataTableState | null>(null)
-
-export function useDataTableContext() {
-  return useContext(DataTableContext)
-}
+const DataTableContext = createContext<DataTableContextType>([initialState, () => {}])
 
 export default function DataTableContextProvider({ config, children }: DataTableContextProviderProps) {
   const init: DataTableState = { ...initialState, ...config }
   const [state, dispatch] = useReducer(reducer, init)
-  const value = useMemo(() => [state, dispatch], [state, dispatch])
+  const value: DataTableContextType = useMemo(() => [state, dispatch], [state, dispatch])
 
   return <DataTableContext.Provider value={value}>{children}</DataTableContext.Provider>
+}
+
+export function useDataTableContext() {
+  return useContext(DataTableContext)
 }
